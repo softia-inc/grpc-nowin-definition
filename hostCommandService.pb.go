d39cee61e4624d8323fe718b429a98ae0174ba3b
tkyatg <takuya.engineer911@gmail.com>
2021-11-10 21:45:40 +0900

    create host

--- hostCommandService.pb.go
+++ hostCommandService.pb.go
@@ -174,6 +174,115 @@ func (x *HostCommandCreateFirstResponse) GetLastName() string {
 	return ""
 }
 
+type HostCommandCreateRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Email     string `protobuf:"bytes,1,opt,name=email,proto3" json:"email,omitempty"`
+	FirstName string `protobuf:"bytes,2,opt,name=firstName,proto3" json:"firstName,omitempty"`
+	LastName  string `protobuf:"bytes,3,opt,name=lastName,proto3" json:"lastName,omitempty"`
+	Password  string `protobuf:"bytes,4,opt,name=password,proto3" json:"password,omitempty"`
+}
+
+func (x *HostCommandCreateRequest) Reset() {
+	*x = HostCommandCreateRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_hostCommandService_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *HostCommandCreateRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*HostCommandCreateRequest) ProtoMessage() {}
+
+func (x *HostCommandCreateRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_hostCommandService_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use HostCommandCreateRequest.ProtoReflect.Descriptor instead.
+func (*HostCommandCreateRequest) Descriptor() ([]byte, []int) {
+	return file_hostCommandService_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *HostCommandCreateRequest) GetEmail() string {
+	if x != nil {
+		return x.Email
+	}
+	return ""
+}
+
+func (x *HostCommandCreateRequest) GetFirstName() string {
+	if x != nil {
+		return x.FirstName
+	}
+	return ""
+}
+
+func (x *HostCommandCreateRequest) GetLastName() string {
+	if x != nil {
+		return x.LastName
+	}
+	return ""
+}
+
+func (x *HostCommandCreateRequest) GetPassword() string {
+	if x != nil {
+		return x.Password
+	}
+	return ""
+}
+
+type HostCommandCreateResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *HostCommandCreateResponse) Reset() {
+	*x = HostCommandCreateResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_hostCommandService_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *HostCommandCreateResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*HostCommandCreateResponse) ProtoMessage() {}
+
+func (x *HostCommandCreateResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_hostCommandService_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use HostCommandCreateResponse.ProtoReflect.Descriptor instead.
+func (*HostCommandCreateResponse) Descriptor() ([]byte, []int) {
+	return file_hostCommandService_proto_rawDescGZIP(), []int{3}
+}
+
 var File_hostCommandService_proto protoreflect.FileDescriptor
 
 var file_hostCommandService_proto_rawDesc = []byte{
@@ -199,17 +308,33 @@ var file_hostCommandService_proto_rawDesc = []byte{
 	0x72, 0x73, 0x74, 0x4e, 0x61, 0x6d, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x09, 0x66,
 	0x69, 0x72, 0x73, 0x74, 0x4e, 0x61, 0x6d, 0x65, 0x12, 0x1a, 0x0a, 0x08, 0x6c, 0x61, 0x73, 0x74,
 	0x4e, 0x61, 0x6d, 0x65, 0x18, 0x04, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x6c, 0x61, 0x73, 0x74,
-	0x4e, 0x61, 0x6d, 0x65, 0x32, 0x72, 0x0a, 0x12, 0x48, 0x6f, 0x73, 0x74, 0x43, 0x6f, 0x6d, 0x6d,
-	0x61, 0x6e, 0x64, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x5c, 0x0a, 0x0b, 0x43, 0x72,
-	0x65, 0x61, 0x74, 0x65, 0x46, 0x69, 0x72, 0x73, 0x74, 0x12, 0x24, 0x2e, 0x6e, 0x6f, 0x77, 0x69,
+	0x4e, 0x61, 0x6d, 0x65, 0x22, 0x86, 0x01, 0x0a, 0x18, 0x48, 0x6f, 0x73, 0x74, 0x43, 0x6f, 0x6d,
+	0x6d, 0x61, 0x6e, 0x64, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
+	0x74, 0x12, 0x14, 0x0a, 0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09,
+	0x52, 0x05, 0x65, 0x6d, 0x61, 0x69, 0x6c, 0x12, 0x1c, 0x0a, 0x09, 0x66, 0x69, 0x72, 0x73, 0x74,
+	0x4e, 0x61, 0x6d, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x09, 0x66, 0x69, 0x72, 0x73,
+	0x74, 0x4e, 0x61, 0x6d, 0x65, 0x12, 0x1a, 0x0a, 0x08, 0x6c, 0x61, 0x73, 0x74, 0x4e, 0x61, 0x6d,
+	0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x6c, 0x61, 0x73, 0x74, 0x4e, 0x61, 0x6d,
+	0x65, 0x12, 0x1a, 0x0a, 0x08, 0x70, 0x61, 0x73, 0x73, 0x77, 0x6f, 0x72, 0x64, 0x18, 0x04, 0x20,
+	0x01, 0x28, 0x09, 0x52, 0x08, 0x70, 0x61, 0x73, 0x73, 0x77, 0x6f, 0x72, 0x64, 0x22, 0x1b, 0x0a,
+	0x19, 0x48, 0x6f, 0x73, 0x74, 0x43, 0x6f, 0x6d, 0x6d, 0x61, 0x6e, 0x64, 0x43, 0x72, 0x65, 0x61,
+	0x74, 0x65, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x32, 0xc1, 0x01, 0x0a, 0x12, 0x48,
+	0x6f, 0x73, 0x74, 0x43, 0x6f, 0x6d, 0x6d, 0x61, 0x6e, 0x64, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63,
+	0x65, 0x12, 0x5c, 0x0a, 0x0b, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x46, 0x69, 0x72, 0x73, 0x74,
+	0x12, 0x24, 0x2e, 0x6e, 0x6f, 0x77, 0x69, 0x6e, 0x2e, 0x48, 0x6f, 0x73, 0x74, 0x43, 0x6f, 0x6d,
+	0x6d, 0x61, 0x6e, 0x64, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x46, 0x69, 0x72, 0x73, 0x74, 0x52,
+	0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x25, 0x2e, 0x6e, 0x6f, 0x77, 0x69, 0x6e, 0x2e, 0x48,
+	0x6f, 0x73, 0x74, 0x43, 0x6f, 0x6d, 0x6d, 0x61, 0x6e, 0x64, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65,
+	0x46, 0x69, 0x72, 0x73, 0x74, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x12,
+	0x4d, 0x0a, 0x06, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x12, 0x1f, 0x2e, 0x6e, 0x6f, 0x77, 0x69,
 	0x6e, 0x2e, 0x48, 0x6f, 0x73, 0x74, 0x43, 0x6f, 0x6d, 0x6d, 0x61, 0x6e, 0x64, 0x43, 0x72, 0x65,
-	0x61, 0x74, 0x65, 0x46, 0x69, 0x72, 0x73, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a,
-	0x25, 0x2e, 0x6e, 0x6f, 0x77, 0x69, 0x6e, 0x2e, 0x48, 0x6f, 0x73, 0x74, 0x43, 0x6f, 0x6d, 0x6d,
-	0x61, 0x6e, 0x64, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x46, 0x69, 0x72, 0x73, 0x74, 0x52, 0x65,
-	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x2e, 0x5a, 0x2c, 0x67, 0x69, 0x74, 0x68,
-	0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x6f, 0x66, 0x74, 0x69, 0x61, 0x2d, 0x69, 0x6e,
-	0x63, 0x2f, 0x6e, 0x6f, 0x77, 0x69, 0x6e, 0x2d, 0x64, 0x65, 0x66, 0x69, 0x6e, 0x69, 0x74, 0x69,
-	0x6f, 0x6e, 0x3b, 0x6e, 0x6f, 0x77, 0x69, 0x6e, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x61, 0x74, 0x65, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x20, 0x2e, 0x6e, 0x6f, 0x77,
+	0x69, 0x6e, 0x2e, 0x48, 0x6f, 0x73, 0x74, 0x43, 0x6f, 0x6d, 0x6d, 0x61, 0x6e, 0x64, 0x43, 0x72,
+	0x65, 0x61, 0x74, 0x65, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x2e,
+	0x5a, 0x2c, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x73, 0x6f, 0x66,
+	0x74, 0x69, 0x61, 0x2d, 0x69, 0x6e, 0x63, 0x2f, 0x6e, 0x6f, 0x77, 0x69, 0x6e, 0x2d, 0x64, 0x65,
+	0x66, 0x69, 0x6e, 0x69, 0x74, 0x69, 0x6f, 0x6e, 0x3b, 0x6e, 0x6f, 0x77, 0x69, 0x6e, 0x62, 0x06,
+	0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -224,16 +349,20 @@ func file_hostCommandService_proto_rawDescGZIP() []byte {
 	return file_hostCommandService_proto_rawDescData
 }
 
-var file_hostCommandService_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_hostCommandService_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
 var file_hostCommandService_proto_goTypes = []interface{}{
 	(*HostCommandCreateFirstRequest)(nil),  // 0: nowin.HostCommandCreateFirstRequest
 	(*HostCommandCreateFirstResponse)(nil), // 1: nowin.HostCommandCreateFirstResponse
+	(*HostCommandCreateRequest)(nil),       // 2: nowin.HostCommandCreateRequest
+	(*HostCommandCreateResponse)(nil),      // 3: nowin.HostCommandCreateResponse
 }
 var file_hostCommandService_proto_depIdxs = []int32{
 	0, // 0: nowin.HostCommandService.CreateFirst:input_type -> nowin.HostCommandCreateFirstRequest
-	1, // 1: nowin.HostCommandService.CreateFirst:output_type -> nowin.HostCommandCreateFirstResponse
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+	2, // 1: nowin.HostCommandService.Create:input_type -> nowin.HostCommandCreateRequest
+	1, // 2: nowin.HostCommandService.CreateFirst:output_type -> nowin.HostCommandCreateFirstResponse
+	3, // 3: nowin.HostCommandService.Create:output_type -> nowin.HostCommandCreateResponse
+	2, // [2:4] is the sub-list for method output_type
+	0, // [0:2] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -269,6 +398,30 @@ func file_hostCommandService_proto_init() {
 				return nil
 			}
 		}
+		file_hostCommandService_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*HostCommandCreateRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_hostCommandService_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*HostCommandCreateResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -276,7 +429,7 @@ func file_hostCommandService_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_hostCommandService_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
@@ -303,6 +456,7 @@ const _ = grpc.SupportPackageIsVersion6
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type HostCommandServiceClient interface {
 	CreateFirst(ctx context.Context, in *HostCommandCreateFirstRequest, opts ...grpc.CallOption) (*HostCommandCreateFirstResponse, error)
+	Create(ctx context.Context, in *HostCommandCreateRequest, opts ...grpc.CallOption) (*HostCommandCreateResponse, error)
 }
 
 type hostCommandServiceClient struct {
@@ -322,9 +476,19 @@ func (c *hostCommandServiceClient) CreateFirst(ctx context.Context, in *HostComm
 	return out, nil
 }
 
+func (c *hostCommandServiceClient) Create(ctx context.Context, in *HostCommandCreateRequest, opts ...grpc.CallOption) (*HostCommandCreateResponse, error) {
+	out := new(HostCommandCreateResponse)
+	err := c.cc.Invoke(ctx, "/nowin.HostCommandService/Create", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // HostCommandServiceServer is the server API for HostCommandService service.
 type HostCommandServiceServer interface {
 	CreateFirst(context.Context, *HostCommandCreateFirstRequest) (*HostCommandCreateFirstResponse, error)
+	Create(context.Context, *HostCommandCreateRequest) (*HostCommandCreateResponse, error)
 }
 
 // UnimplementedHostCommandServiceServer can be embedded to have forward compatible implementations.
@@ -333,6 +497,9 @@ type UnimplementedHostCommandServiceServer struct {
 
 func (*UnimplementedHostCommandServiceServer) CreateFirst(context.Context, *HostCommandCreateFirstRequest) (*HostCommandCreateFirstResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateFirst not implemented")
+}
+func (*UnimplementedHostCommandServiceServer) Create(context.Context, *HostCommandCreateRequest) (*HostCommandCreateResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Create not implemented")
 }
 
 func RegisterHostCommandServiceServer(s *grpc.Server, srv HostCommandServiceServer) {
@@ -357,6 +524,24 @@ func _HostCommandService_CreateFirst_Handler(srv interface{}, ctx context.Contex
 	return interceptor(ctx, in, info, handler)
 }
 
+func _HostCommandService_Create_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(HostCommandCreateRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(HostCommandServiceServer).Create(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/nowin.HostCommandService/Create",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(HostCommandServiceServer).Create(ctx, req.(*HostCommandCreateRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _HostCommandService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "nowin.HostCommandService",
 	HandlerType: (*HostCommandServiceServer)(nil),
@@ -364,6 +549,10 @@ var _HostCommandService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "CreateFirst",
 			Handler:    _HostCommandService_CreateFirst_Handler,
+		},
+		{
+			MethodName: "Create",
+			Handler:    _HostCommandService_Create_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
